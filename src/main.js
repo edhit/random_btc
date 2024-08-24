56a99e868b5b3760e4df7e0616d252425573d90e
John Doe <johndoe@example.com>
2024-08-25 00:30:52 +0300

    new file:   .env.example 	modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,35 +1,67 @@
+require("dotenv").config();
+
 const CoinKey = require("coinkey");
+const { Telegraf } = require("telegraf");
 const logger = require("./logger");
 const fs = require("fs");
+const moment = require("moment");
+
+const token = process.env.TOKEN ? process.env.TOKEN : "token";
+const chat_id = process.env.CHAT_ID ? process.env.CHAT_ID : 1;
+
+const bot = new Telegraf(token);
 
 async function getAddress(path) {
-	const data = fs.readFileSync(path).toString().split("\r\n");
+	try {
+		const data = fs.readFileSync(path).toString().split("\r\n");
 
-	return data;
+		return data;
+	} catch (error) {
+		await bot.telegram.sendMessage(chat_id, error);
+		return false;
+	}
 }
 
 async function check(data, ck) {
-	if (data.includes(`${ck.publicAddress}\\n`)) {
-		console.log("Private Key (Wallet Import Format): " + ck.privateWif);
-		console.log("Private Key (Hex): " + ck.privateKey.toString("hex"));
-		console.log("Address: " + ck.publicAddress);
-		logger.info("Private Key (Wallet Import Format): " + ck.privateWif);
-		logger.info("Private Key (Hex): " + ck.privateKey.toString("hex"));
-		logger.info("Address: " + ck.publicAddress);
+	try {
+		if (data.includes(`${ck.publicAddress}\\n`)) {
+			console.log("Private Key (Wallet Import Format): " + ck.privateWif);
+			console.log("Private Key (Hex): " + ck.privateKey.toString("hex"));
+			console.log("Address: " + ck.publicAddress);
+			logger.info("Private Key (Wallet Import Format): " + ck.privateWif);
+			logger.info("Private Key (Hex): " + ck.privateKey.toString("hex"));
+			logger.info("Address: " + ck.publicAddress);
+
+			await bot.telegram.sendMessage(
+				chat_id,
+				`${ck.publicAddress} - ${
+					ck.privateWif
+				} - ${ck.privateKey.toString("hex")}`
+			);
+		}
+	} catch (error) {
+		await bot.telegram.sendMessage(chat_id, error);
+		return false;
 	}
 }
 
-exports.main = async function (attempt = 1000000) {
-	const data = await getAddress("./data.txt");
+exports.main = async function (attempt = 1000) {
+	try {
+		const data = await getAddress("./data.txts");
+		if (!data) return false;
 
-	for (let index = 0; index < attempt; index++) {
-		let ck = new CoinKey.createRandom();
-		await check(data, ck);
-		ck.compressed = false;
-		await check(data, ck);
+		for (let index = 0; index < attempt; index++) {
+			let ck = new CoinKey.createRandom();
+			await check(data, ck);
+			ck.compressed = false;
+			await check(data, ck);
+		}
+		const date = moment().format("MMM Do YY, h:mm:ss");
+
+		console.log(date);
+		logger.info(date);
+	} catch (error) {
+		await bot.telegram.sendMessage(chat_id, error);
+		return false;
 	}
-	const date = new Date().toString();
-
-	console.log(date);
-	logger.info(date);
 };
