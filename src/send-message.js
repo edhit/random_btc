b3d2b6d62273640a551641591dc038b68622d772
edhit <46953444+edhit@users.noreply.github.com>
2024-11-12 19:14:47 +0300

    Update send-message.js

--- src/send-message.js
+++ src/send-message.js
@@ -2,6 +2,7 @@ const { Telegraf } = require("telegraf");
 
 const token = process.env.TOKEN ? process.env.TOKEN : "token";
 const chat_id = process.env.CHAT_ID ? process.env.CHAT_ID : 1;
+const logger = require("./logger");
 
 const bot = new Telegraf(token);
 
